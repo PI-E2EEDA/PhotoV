5b55900508a8d866db765419e9c6b35adac14081
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-03-23 00:31:16 +0100

    Configure alembic for postgres

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,30 +6,35 @@ from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
 from sqlalchemy.orm import sessionmaker
 import os
 
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-POSTGRES_USER = os.environ.get("POSTGRES_USER")
-POSTGRES_DB = os.environ.get("POSTGRES_DB")
-DB_HOST = os.environ.get("DB_HOST", "localhost")
 
-url = URL.create(
-    drivername="postgresql+psycopg",
-    username=POSTGRES_USER,
-    password=POSTGRES_PASSWORD,
-    host=DB_HOST,
-    port=5432,
-    database=POSTGRES_DB,
-)
+def get_database_url(use_async: bool) -> str:
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "demo")
+    POSTGRES_USER = os.environ.get("POSTGRES_USER", "photov")
+    POSTGRES_DB = os.environ.get("POSTGRES_DB", "photov")
+    DB_HOST = os.environ.get("DB_HOST", "localhost")
 
-engine = AsyncEngine(create_engine(url, echo=True, future=True))
-
-
-async def init_db():
-    async with engine.begin() as conn:
-        # await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
+    return URL.create(
+        # Note: it seems that fastapi requires to use an async runtime
+        # but alembic require it to be sync, so we allow the choice via use_async
+        drivername="postgresql+" + ("asyncpg" if use_async else "psycopg"),
+        username=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=DB_HOST,
+        port=5432,
+        database=POSTGRES_DB,
+    ).render_as_string(hide_password=False)
 
 
-async def get_session() -> AsyncSession:
-    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-    async with async_session() as session:
-        yield session
+# engine = AsyncEngine(create_engine(get_database_url(), echo=True, future=True))
+
+#
+# async def init_db():
+#     async with engine.begin() as conn:
+#         # await conn.run_sync(SQLModel.metadata.drop_all)
+#         await conn.run_sync(SQLModel.metadata.create_all)
+#
+#
+# async def get_session() -> AsyncSession:
+#     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+#     async with async_session() as session:
+#         yield session
--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -3,11 +3,18 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from sqlmodel import SQLModel
+
 from alembic import context
+
+from app.db import get_database_url
+from app.models import EnergyMeasure, PowerMeasure, Building
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", get_database_url(False))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +71,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
