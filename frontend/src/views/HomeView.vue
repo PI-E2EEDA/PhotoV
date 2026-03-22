630c26b08b71a42d1f1915a48cca5ebd1aa8d154
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-03-22 18:32:45 +0100

    Add logo to basic HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex h-[80vh] justify-center items-center text-center">
+    <div>
+      <img alt="Vue logo" class="h-32 my-10" src="@/assets/logo.svg" />
+      <h1 class="text-4xl">Solar panels</h1>
+    </div>
+  </div>
 </template>
