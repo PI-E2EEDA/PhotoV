bddb770ca6eb44c1207d269499b60f7bfc6fbc69
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-03-22 18:44:34 +0100

    Better tagline on HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,10 +2,12 @@
 </script>
 
 <template>
-  <div class="flex h-[80vh] justify-center items-center text-center">
-    <div>
-      <img alt="Vue logo" class="h-32 my-10" src="@/assets/logo.svg" />
-      <h1 class="text-4xl">Solar panels</h1>
-    </div>
+  <div class="flex flex-col h-[80vh] justify-center items-center text-center">
+    <img alt="Vue logo" class="h-32 my-10" src="@/assets/logo.svg" />
+    <h1 class="text-4xl max-w-1/2 font-semibold">Optimize your auto consumption to maximize the impact of your solar
+      installation.
+    </h1>
+
+    <a class="text-xl my-3" href="https://github.com/PI-E2EEDA/PhotoV">See Git repository on GitHub</a>
   </div>
 </template>
