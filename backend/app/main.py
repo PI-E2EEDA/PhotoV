c93c99f5a5c6486dbade7218366c0c91b8418fb6
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-03-26 23:21:53 +0100

    Add validate_current_user_can_access_installation() for /measures

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,12 @@
-import os
-from typing import Annotated, Optional
+from typing import Annotated
 
-from fastapi import Depends, FastAPI, Request, Response
-from fastapi_users import BaseUserManager, FastAPIUsers, IntegerIDMixin
-from fastapi_users.authentication import (
-    AuthenticationBackend,
-    BearerTransport,
-)
-from sqlalchemy.ext.asyncio import create_async_engine
+from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.auth import setup_auth_routes
-from app.db import get_database_strategy, get_session, get_user_db
-from app.models import Measure, MeasureType, User
+from app.db import get_session
+from app.models import Installation, Measure, MeasureType, User, UserInstallationLink
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 app = FastAPI()
@@ -28,17 +21,59 @@ async def root():
     }
 
 
-# The logged user must always be active and verified !!
-current_user = fastapi_users_setup.current_user(active=True, verified=True)
+# This defines a function to call to get the current user
+# The logged user can get data only when active and verified !!
+current_user_fn = fastapi_users_setup.current_user(
+    optional=False, active=True, verified=True
+)
 
 
-@app.get("/measures/{installation_id}/{type}")
+# Make sure the user can access data from this installation or throw an HTTP exception otherwise
+async def validate_current_user_can_access_installation(
+    user_id,
+    installation_id,
+    session: SessionDep,
+) -> Installation:
+    result = await session.execute(
+        select(UserInstallationLink).where(
+            UserInstallationLink.user_id == user_id,
+            UserInstallationLink.installation_id == installation_id,
+        )
+    )
+    link = result.scalars().first()
+    if link is None:
+        raise HTTPException(
+            status_code=403,
+            detail=f"Cannot access to installation with id {installation_id}",
+        )
+
+    result2 = await session.execute(
+        select(Installation).where(
+            Installation.id == installation_id,
+        )
+    )
+    installation = result2.scalars().first()
+    # This should be unreachable...
+    if installation is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Installation with id {installation_id} has been deleted...",
+        )
+    return installation
+
+
+@app.get(
+    "/measures/{installation_id}/{type}", description="Get measures of power or energy"
+)
 async def get_measures(
     session: SessionDep,
     installation_id: int,
     type: MeasureType,
-    user: User = Depends(current_user),
+    user: User = Depends(current_user_fn),
 ):
+    await validate_current_user_can_access_installation(
+        user.id, installation_id, session
+    )
     print(f"request as user {user.email}")
     results = await session.execute(  # ignore this warning
         select(Measure)
