8587388a5411d7ff294bcc4671d60b7d69427a75
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-04-12 18:54:50 +0200

    Save WIP of new routes for smartplugs and associated measures

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,15 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.auth import setup_auth_routes
 from app.db import get_session
-from app.models import Installation, Measure, MeasureType, User, UserInstallationLink
+from app.models import (
+    Installation,
+    Measure,
+    MeasureType,
+    User,
+    UserInstallationLink,
+    SmartPlugMeasure,
+    SmartPlug,
+)
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 app = FastAPI()
@@ -82,3 +90,60 @@ async def get_measures(
         .limit(3)
     )
     return results.scalars().all()
+
+
+# TODO: add routes to get smartplugs and smartplugs measures for the frontend
+
+
+@app.post(
+    "/smartplugs/",
+    description="Create a new smartplug with a name",
+)
+async def create_smartplug(
+    session: SessionDep,
+    smartplug: SmartPlug,
+    user: User = Depends(current_user_fn),
+):
+    await validate_current_user_can_access_installation(
+        user.id, smartplug.installation_id, session
+    )
+    smartplug.id = None
+
+    ## TODO insert
+    ## TODO return the generated object
+
+
+@app.post(
+    "/smartplugs/{installation_id}/",
+    description="Send one power measure from a given smart-plug",
+)
+async def send_smartplug_measure(
+    session: SessionDep,
+    installation_id: int,
+    measure: SmartPlugMeasure,
+    user: User = Depends(current_user_fn),
+):
+    await validate_current_user_can_access_installation(
+        user.id, installation_id, session
+    )
+    # Check the smartplug_id does exist and can be accessed
+    related_smartplug = await session.execute(  # ignore this warning
+        select(SmartPlug).where(SmartPlug.id == measure.smartplug_id)
+    )
+    if related_smartplug.first() is None:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid smartplug_id for this measure !",
+        )
+    if related_smartplug.first() is None:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid smartplug_id for this measure !",
+        )
+
+    measure.id = None
+    # Check the time is not in the future
+    # Check the value is not negative
+    print(f"Request to save new smartplug value")
+
+    # todo return new object
