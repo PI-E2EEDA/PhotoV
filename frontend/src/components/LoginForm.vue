2053d0d83d4637acd408f7f33c97ace8e8b16d87
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-04-13 00:43:50 +0200

    Add some basic default style and design login form

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref("")
+const password = ref("")
+
+function submit() {
+  console.log("logging as " + email.value)
+}
 </script>
 
 <template>
-  LOGIN TODO
+  <div class="max-w-60 m-auto h-[80vh] flex items-center">
+    <div>
+      <div>
+        <label>Email</label>
+        <input type="email" v-model="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" v-model="password" />
+      </div>
+
+      <button @click="submit">Login</button>
+    </div>
+  </div>
 </template>
