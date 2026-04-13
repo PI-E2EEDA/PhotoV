4d67525412c11b0b25400ce341355723c72721ce
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-04-13 12:54:35 +0200

    Fix menu with logged state

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useApiStore } from './stores/api';
+
+const api = useApiStore()
 </script>
 
 <template>
   <div class="p-2">
     <header class="flex py-2 border-b border-logo-p/30">
       <div class="flex-1 p-2">
-        <img alt="Vue logo" class="h-5" src="@/assets/logo.svg" />
+        <img alt="Vue logo" class="h-7" src="@/assets/logo.svg" />
       </div>
 
       <nav class="flex gap-3 items-center">
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink v-if="!api.logged" to="/login">Login</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <a v-if="api.logged" @click="api.logout()">Logout</a>
       </nav>
     </header>
 
