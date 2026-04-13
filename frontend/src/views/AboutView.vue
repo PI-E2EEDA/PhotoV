337a99e74b4800e35402f73f510b0e760895333b
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-04-13 12:59:59 +0200

    Add section in About page to copy current API token

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,21 @@
+<script setup>
+import { useApiStore } from '@/stores/api';
+
+const api = useApiStore()
+function copyToken() {
+  navigator.clipboard.writeText(api.token);
+  alert("Done !")
+}
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+
+    <div>
+      <h3>Access the current API token</h3>
+      <p class="font-bold">A token is as sensible as a password, make sure to keep it secret !</p>
+      <p>Note: when you logged out here it will be revoked !</p>
+      <button @click="copyToken">Copy to clipboard</button>
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
