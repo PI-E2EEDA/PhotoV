e483385a85c9f1a4d11d56affcb75fc4fb9db889
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-04-30 18:28:10 +0200

    Add first version of Credits section in About page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,17 @@
-<script setup>
-import { useApiStore } from '@/stores/api';
+<script setup lang="ts">
+import { useApiStore } from '@/stores/api'
 
 const api = useApiStore()
 function copyToken() {
-  navigator.clipboard.writeText(api.token);
-  alert("Done !")
+  navigator.clipboard.writeText(api.token)
+  alert('Done !')
 }
 </script>
 <template>
   <div class="about">
     <h1>About</h1>
+
+    You are logged in with account TODO and here are your tokens. hide if logged out ! transform as /user logout button without revoking token
 
     <div>
       <h3>Access the current API token</h3>
@@ -17,5 +19,18 @@ function copyToken() {
       <p>Note: when you logged out here it will be revoked !</p>
       <button @click="copyToken">Copy to clipboard</button>
     </div>
+  </div>
+
+  <div>
+    <h2>Credits</h2>
+    The data displayed in PhotoV is coming from various sources:
+    <ul>
+      <li><strong>MeteoSwiss</strong> for the solar prediction and weather history. <a href="https://opendatadocs.meteoswiss.ch/e-forecast-data/e4-local-forecast-data">See docs</a></li>
+      <li>
+        <strong>SolarEdge</strong> is the source of measures for solar consumption, solar and grid consumption.
+        <a href="https://knowledge-center.solaredge.com/sites/kc/files/se_monitoring_api.pdf">See their API docs</a>
+      </li>
+      <li><strong>Smartplugs data</strong> is provided by our projects using Swissdomotics smartplugs.</li>
+    </ul>
   </div>
 </template>
--- frontend/src/views/SmartplugsView.vue
+++ frontend/src/views/SmartplugsView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import SmartplugsDetails from '@/components/SmartplugsDetails.vue';
+import SmartplugsDetails from '@/components/SmartplugsDetails.vue'
 </script>
 
 <template>
