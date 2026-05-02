27887690e30938e18ec8dd12c7f05e47a6e15b24
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-05-02 15:08:48 +0200

    Integrate baseline subtraction for virtual smartplugs

--- data-acquisition/virtual/start-virtual-smartplug.py
+++ data-acquisition/virtual/start-virtual-smartplug.py
@@ -7,9 +7,10 @@ from pathlib import Path
 
 VIRTUAL_CONFIG_PATH = Path(__file__).parent / "virtual.json"
 API_ROUTE_SEND_MEASURE = "smartplugs"
-# IMPORTANT: this is not the same subdomain !
+# IMPORTANT: this is not the same subdomain as the official monitoring API !
 SOLAREDGE_INTERNAL_API_BASE_URL = "https://monitoring.solaredge.com"
 PHOTOV_API_BASE_URL = "https://api.photov.srd.rs"
+SAVE_INTERVAL_S = 10  # like the physical smartplugs
 
 
 def get_virtual_config():
@@ -19,7 +20,7 @@ def get_virtual_config():
 
 # This request by built by looking at the Firefox Network panel (F12) on the monitoring UI.
 # When filtering with "power-flow", we can see a request every 5-6 seconds. With a right-click, copy, copy as CURL, we can extract all the headers.
-def get_powerflow_measure_from_solaredge(inst_config):
+def get_powerflow_measure_from_solaredge(log_result, inst_config):
     site_id = inst_config["solaredge_site_id"]
     cookies = inst_config["cookies"]
 
@@ -46,10 +47,13 @@ def get_powerflow_measure_from_solaredge(inst_config):
             # WARNING: the value is in in kW, we need to convert it !
             current_power_kw = float(data["consumption"]["currentPower"])
             current_power_w = 1000 * current_power_kw
-            print(f"Current power is {current_power_w}W: ", end="", flush=True)
+            if log_result:
+                now = datetime.now()
+                formatted = now.strftime("%Y-%m-%d %H:%M:%S")
+                print(f"{formatted}: Power is {current_power_w}W, ", end="", flush=True)
             return current_power_w
         else:
-            print(f"Request failed with code {response.status_code}")
+            log_result(f"Request failed with code {response.status_code}")
     return None
 
 
@@ -71,7 +75,7 @@ def save_smartplug_measure_on_photov(inst_config, smartplug_id: int, value: floa
                 },
             )
             if response.status_code == 200:
-                print(f"sent on smartplug {smartplug_id}")
+                print(f"sent adapted measure {value}W for smartplug {smartplug_id}")
             else:
                 print(
                     f"Smartplug {smartplug_id}: API error {response.status_code} : {response.text}"
@@ -103,11 +107,36 @@ def interactive_start():
 
 def main():
     inst_config, smartplug_id = interactive_start()
+    print(
+        "Measuring current power flow to determine the baseline power to substract to measures"
+    )
+    print(
+        "If the physical device has already started, you may need to manually take the power flow value before its activation."
+    )
+    baseline_value = get_powerflow_measure_from_solaredge(False, inst_config)
+    if baseline_value is None:
+        print("Error: could not get current powerflow !")
+        return
+    confirmed = questionary.confirm(
+        f"Do you want to take this baseline of {baseline_value}W ?",
+        default=False,
+    ).ask()
+
+    if not confirmed:
+        manual_baseline = questionary.text(
+            "Can you enter your own baseline in W please ? "
+        ).ask()
+        baseline_value = float(manual_baseline)
+
     while True:
-        value = get_powerflow_measure_from_solaredge(inst_config)
+        value = get_powerflow_measure_from_solaredge(True, inst_config)
         if value is not None:
+            value = value - baseline_value
+            # We need to make sure we don't go into the negative if the real baseline is going lower what we chose
+            if value < 0:
+                value = 0
             save_smartplug_measure_on_photov(inst_config, smartplug_id, value)
-        sleep(10)
+        sleep(SAVE_INTERVAL_S)
 
 
 main()
